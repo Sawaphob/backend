b949c9ac645ec870ebf7f080cbb7b6f95ad5f098
unknown <magician_TH@hotmail.com>
2019-03-19 22:17:47 +0700

    SocketIO with some templates

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+// const express = require('express');
+const APP_PORT = 4000;
+const io = require('socket.io').listen(APP_PORT);
+
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const Group = require('./models/group');
 const JoinedGroupInfo = require('./models/groupjoinedinfo');
 const Message = require('./models/message');
 
-const APP_PORT = 4000;
 
 // DB ---------------------------------------------------------------------------
 mongoose.connect('mongodb://localhost/test'); // test =  database name
@@ -14,18 +16,80 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('DB connected!')});
 //--------------------------------------
 var aUser = new User({ name: 'userNameKub' });
-aUser.save();
 var aGroup = new Group({ name: 'Group101kub' });
 var aUser2 = new User({ name: 'user2NameKub' });
-
 //-----------------------------------------------------------------------------
 
-var app = express();
-var server = app.listen(APP_PORT, () => console.log('Listening on APP_PORT ' + APP_PORT));
-app.get('/', (req, res) => {
-  User.find({}, function (err, users) {
-    if (err) return console.error(err);
-    console.log(users);
-    res.send(users); // for go to localhost:4000 and see data in browser
+function userEnter(data) {
+  // TODO [DB] : Create user if not existed
+
+}
+
+function GetAndSendAllChats() {
+  // TODO [DB] : Get All chats and send back
+  var allChats = { /* QUERYed */
+    "Group1" : [
+      {
+        username: "This",
+        content: <p>This</p>,
+        timeStamp: "1:23"
+      },
+      {
+        username: "is",
+        content: <p>is</p>,
+        timeStamp: "2:34"
+      },
+      {
+        username: "Group1",
+        content: <p>group1</p>,
+        timeStamp: "3.45"
+      },
+    ],
+    "Group2" : [
+      {
+        username: "This",
+        content: <p>This</p>,
+        timeStamp: "1:23"
+      },
+      {
+        username: "is",
+        content: <p>is</p>,
+        timeStamp: "2:34"
+      }
+    ]
+  }
+  socket.emit('Allchat',allChats); 
+}
+
+function storeMessage(message) {
+  // TODO [DB] : Store message in DB !
+}
+
+io.on('connection', function (socket) {
+  io.emit('this', { will: 'be received by everyone'});
+
+  // After click enter button , data = username 
+  socket.on('enter', function (data) {
+    console.log('Received [enter] event!');
+    console.log(data);  
+    userEnter(data);
+    GetAndSendAllChats();
+  });
+  
+  socket.on('sendMessage', function(data){
+    console.log('Received [enter] event!');
+    console.log(data);
+    var dummyMessage = {}
+    storeMessage();
+    socket.emit('updateSendMessages',function(datakub) {
+      /* Send Messages to others in chat */
+      /* Message must be TOTAL ORDER something -- maybe store all message in DB and query ALL message in TOTAL ORDER and sendback?  */
+      // see more -- broadcast , but tun: think wa mai na ja work
+    });
   })
-})
+
+  socket.on('disconnect', function () {
+    io.emit('a user disconnected');
+  });
+});
+
