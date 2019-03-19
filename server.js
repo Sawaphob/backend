93a70a6ec1bc3d0e658eca07bc52cd88b79a3e41
Sawaphob Chavana <sawaphob@hotmail.com>
2019-03-19 13:06:33 +0700

    initail socket and add function

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const User = require('./models/user');
 const Group = require('./models/group');
 const JoinedGroupInfo = require('./models/groupjoinedinfo');
 const Message = require('./models/message');
-
+const socket = require('socket.io');
 const APP_PORT = 4000;
 
 // DB ---------------------------------------------------------------------------
@@ -19,7 +19,6 @@ var aGroup = new Group({ name: 'Group101kub' });
 var aUser2 = new User({ name: 'user2NameKub' });
 
 //-----------------------------------------------------------------------------
-
 var app = express();
 var server = app.listen(APP_PORT, () => console.log('Listening on APP_PORT ' + APP_PORT));
 app.get('/', (req, res) => {
@@ -29,3 +28,13 @@ app.get('/', (req, res) => {
     res.send(users); // for go to localhost:4000 and see data in browser
   })
 })
+// Initail Socket.io and implement feature ----------------------------------------------------------
+io = socket(server);
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on('SEND_MESSAGE', function(data){
+        io.emit('RECEIVE_MESSAGE', data);
+    })
+});
+
+
